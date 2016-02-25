758f1500a245551e0289212d4aea7125391bc8ab
Sujay Khandekar <khandek2@illinois.edu>
2016-02-24 23:23:00 -0600

    more changes

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 
 var express = require('express');
 var app = express();
+var sentiment = require('sentiment');
+var fs = require("fs");
+/*
+    Sentiment Analysis
+*/
+var files = [
+	"public/gsw_data/gsw_1.json",
+	"public/gsw_data/gsw_2.json",
+	"public/gsw_data/gsw_3.json",
+	"public/gsw_data/gsw_4.json",
+	"public/gsw_data/gsw_5.json",
+	"public/gsw_data/gsw_6.json",
+	"public/gsw_data/gsw_7.json",
+	"public/gsw_data/gsw_8.json",
+	"public/gsw_data/gsw_9.json",
+	"public/gsw_data/gsw_10.json"
+]
+var geostuff = [];
+for (var j = 0; j < files.length; j++) {
+    var contents = fs.readFileSync(files[j]);
+    var data = JSON.parse(contents);
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].geo.coordinates[0] >= 25
+            && data[i].geo.coordinates[0] <= 50
+            && data[i].geo.coordinates[1] >= -125
+            && data[i].geo.coordinates[1] <= -65) {
+        var s = sentiment(data[i].text);
+        var positive = false;
+        if (s.score > 0) {
+            positive = true;
+        }
+        geostuff.push(
+            {
+                "lat": data[i].geo.coordinates[0],
+                "lon": data[i].geo.coordinates[1],
+                "positive": positive,
+                "days_ago": twitterDate(data[i].created_at)
+            });
+        }
+    }
+}
+
+function twitterDate(tdate) {
+    var system_date = new Date(Date.parse(tdate));
+    var user_date = new Date();
+    var diff = Math.floor((user_date - system_date) / 1000);
+    return Math.round(diff / 86400);
+}
+
+app.post('/tweets', function(req, res){
+   res.send(geostuff);
+});
 
 app.use(express.static(__dirname + '/public'));
 
--- public/script.js
+++ public/script.js
@@ -1,42 +1,5 @@
-/*
-** Data Formatting
-*/
-
-var files = [
-	"gsw_data/gsw_1.json",
-	"gsw_data/gsw_2.json",
-	"gsw_data/gsw_3.json",
-	"gsw_data/gsw_4.json",
-	"gsw_data/gsw_5.json",
-	"gsw_data/gsw_6.json",
-	"gsw_data/gsw_7.json",
-	"gsw_data/gsw_8.json",
-	"gsw_data/gsw_9.json",
-	"gsw_data/gsw_10.json"
-]
-var geostuff = [];
-for (var j = 0; j < files.length; j++) {
-	$.getJSON( files[j], function(data) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].geo.coordinates[0] >= 25
-				&& data[i].geo.coordinates[0] <= 50
-				&& data[i].geo.coordinates[1] >= -125
-				&& data[i].geo.coordinates[1] <= -65) {
-			geostuff.push(
-				{
-					"lat": data[i].geo.coordinates[0],
-					"lon": data[i].geo.coordinates[1]
-				});
-			}
-		}
-	});
-}
-
-console.log(JSON.stringify(geostuff));
-
-/*
-** D3 Stuff
-*/
+var currDate = 1;
+var finalArray = [];
 
 //Width and height
 var w = window.innerWidth * (8/10);
@@ -57,28 +20,69 @@ var svg = d3.select("body")
 			.attr("height", h);
 
 d3.json("us-states.json", function(json) {
-  svg.selectAll("path")
-           .data(json.features)
-           .enter()
-           .append("path")
-           .attr("d", path)
-           .style("fill", "#D3D3D3");
+	svg.selectAll("path")
+	   .data(json.features)
+	   .enter()
+	   .append("path")
+	   .attr("d", path)
+	   .style("fill", "#D3D3D3");
 
-	d3.csv("data.csv", function(data) {
-    	// Dots on the Map
-		svg.selectAll("circle")
-		   .data(data)
-		   .enter()
-		   .append("circle")
-		   .attr("cx", function(d) {
-			   return projection([d.lon, d.lat])[0];
-		   })
-		   .attr("cy", function(d) {
-			   return projection([d.lon, d.lat])[1];
-		   })
-		   .attr("r", 5)
-		   .style("fill", "blue")
-		   .style("opacity", 0.4);
+	   $.ajax( {
+	         url: '/tweets',
+	         type: 'POST',
+	         success: function(geostuff) {
+				var i;
+				for (i in geostuff) {
+					if (!finalArray[geostuff[i].days_ago]) {
+						finalArray[geostuff[i].days_ago] = []
+					}
+					finalArray[geostuff[i].days_ago].push(geostuff[i]);
+				}
 
-	});
+				// Add dots
+		 	    a();
+	         }
+	   });
 });
+
+$('#slider').on("change", function() {
+    console.log('lOL')
+});
+
+var a = function () {
+	var geostuff = finalArray[currDate];
+
+	var positiveTweets = geostuff.filter(function (value) {
+		return value.positive;
+	})
+	var negativeTweets = geostuff.filter(function (value) {
+		return !value.positive;
+	})
+
+   svg.selectAll("circle")
+	  .data(positiveTweets)
+	  .enter()
+	  .append("circle")
+	  .attr("cx", function(d) {
+	   return projection([d.lon, d.lat])[0];
+	  })
+	  .attr("cy", function(d) {
+	   return projection([d.lon, d.lat])[1];
+	  })
+	  .attr("r", 15)
+	  .style("fill", "blue")
+	  .style("opacity", 0.2);
+  svg.selectAll("circle")
+	 .data(negativeTweets)
+	 .enter()
+	 .append("circle")
+	 .attr("cx", function(d) {
+	  return projection([d.lon, d.lat])[0];
+	 })
+	 .attr("cy", function(d) {
+	  return projection([d.lon, d.lat])[1];
+	 })
+	 .attr("r", 15)
+	 .style("fill", "red")
+	 .style("opacity", 0.2);
+};
